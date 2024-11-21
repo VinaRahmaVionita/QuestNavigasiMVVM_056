9b4202ff88b5831b87ea4943b5c6b4013633cfee
vinarahmavionita <vinarahmavionita824@gmail.com>
2024-11-21 15:39:42 +0700

    membuat objek jenis kelamin

---
+++ app/src/main/java/com/example/project5_mvvm/model/DataJenisKelamin.kt
@@ -0,0 +1,10 @@
+package com.example.project5_mvvm.model
+
+import com.example.project5_mvvm.R
+
+object DataJenisKelamin {
+    val listJk = listOf(
+        R.string.laki,
+        R.string.wanita,
+    )
+}
