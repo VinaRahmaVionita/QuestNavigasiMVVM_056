2af3a04671ae6e2e45de078f6c384ec65aaa2b33
vinarahmavionita <vinarahmavionita824@gmail.com>
2024-11-21 15:49:01 +0700

    mendeklarasikan class mewakili ViewModel

---
+++ app/src/main/java/com/example/project5_mvvm/ui/viewmodel/MahasiswaViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.project5_mvvm.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.navigation.NavBackStackEntry
+import com.example.project5_mvvm.model.DataMahasiswa
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class MahasiswaViewModel : ViewModel() {
+
+}
